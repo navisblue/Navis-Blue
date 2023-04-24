84cbc4432e1ebd3fbe8a3b932bcadabbccf62a12
Levan Beroshvili <v-levanbe@microsoft.com>
2023-04-24 13:39:21 +0400

    Created new nextjs project

---
+++ src/pages/_app.tsx
@@ -0,0 +1,6 @@
+import '@navis-blue/styles/globals.css'
+import type { AppProps } from 'next/app'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
